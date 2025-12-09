44b8cb097841c3bf7d3b3f5f170ff9d690e3522e
Bekasyl Asylbekov <basylbekov@crystalspring.kz>
2025-12-09 23:04:15 +0500

    FEA: В проект добавлен API Gateway Server

---
+++ gatewayserver/src/test/java/com/bekassyl/gatewayserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bekassyl.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
