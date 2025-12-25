3c303b789f471a0a599d76e69db237f05236d57b
Bekasyl Asylbekov <basylbekov@crystalspring.kz>
2025-12-25 16:12:54 +0500

    FIX: Исправлен баг с Correlation ID. Добавлена проверка на дублирование header

--- gatewayserver/src/main/java/com/bekassyl/gatewayserver/filter/ResponseTraceFilter.java
+++ gatewayserver/src/main/java/com/bekassyl/gatewayserver/filter/ResponseTraceFilter.java
@@ -18,13 +18,16 @@ public class ResponseTraceFilter {
 
     @Bean
     public GlobalFilter postGlobalFilter() {
-        return (exchange, chain) -> {
-            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
-                String correlationId = filterUtility.getCorrelationId(requestHeaders);
-                logger.debug("Updated the correlation id to the outbound headers: {}", correlationId);
-                exchange.getResponse().getHeaders().add(FilterUtility.CORRELATION_ID, correlationId);
-            }));
-        };
+        return (exchange, chain) ->
+                chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                    HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+                    String correlationId = filterUtility.getCorrelationId(requestHeaders);
+
+                    if (!(exchange.getResponse().getHeaders().containsKey(FilterUtility.CORRELATION_ID))) {
+                        logger.debug("Updated the correlation id to the outbound headers: {}", correlationId);
+                        exchange.getResponse().getHeaders().add(FilterUtility.CORRELATION_ID, correlationId);
+                    }
+                }));
     }
 }
+
